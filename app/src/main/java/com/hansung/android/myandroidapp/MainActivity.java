2c6d54897bc60b4e6d9d2b80230cc2723ac5e429
yudini <oyj0922@naver.com>
2021-03-29 13:34:27 +0900

    이번달 달력 정보

--- app/src/main/java/com/hansung/android/myandroidapp/MainActivity.java
+++ app/src/main/java/com/hansung/android/myandroidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hansung.android.myandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,9 +14,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,32 +28,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_gridview);
 
-        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        TextView todayDate = (TextView) findViewById(R.id.date);
-        Calendar today = Calendar.getInstance();
-        int year = today.get(Calendar.YEAR);
-        int month = today.get(Calendar.MONTH);
-        todayDate.setText(year + "년" + month+1 + "월");
+        Button pre = findViewById(R.id.button);
+        Button next = findViewById(R.id.button2);
 
+        pre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // 데이터 원본 준비
-        String[] items = {"1", "2", "3", "4", "5", "6", "7", "8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+        ArrayList<String> list = new ArrayList<>();
+
+        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        // id를 바탕으로 화면 레이아웃에 정의된 TextView 객체 로딩
+        TextView todayDate = (TextView) findViewById(R.id.date);
+        //현재 날짜 가져오기.
+        Calendar today = Calendar.getInstance();
+        Calendar thisMonth = Calendar.getInstance();
+        thisMonth.set(Calendar.DAY_OF_MONTH,1);     //현재 달의 첫번째날로 날짜 설정
+        int year = today.get(Calendar.YEAR);         //현재년도 정보 얻어서 저장
+        int month = today.get(Calendar.MONTH) +1;    //현재 월의 정보 얻어서 저장
+        int lastDate = today.getActualMaximum(Calendar.DATE);  //이번달의 마지막 날 얻어서 저장
+        int startDate = thisMonth.get(Calendar.DAY_OF_WEEK);   //이번달의 시작요일 얻어서 저장
+        for(int i=0;i<startDate-1;i++){
+            list.add("");                //공백으로 채우기
+        }
+        for(int i=1;i<=lastDate;i++){
+            list.add(Integer.toString(i));     //일 채우기
+        }
+        todayDate.setText(year + "년" + month + "월");    //텍스트뷰에 현재년도 및 월 띄우기
+
+
 
         //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
         ArrayAdapter<String> adapt
                 = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
-                items);
+                list);
 
         // 어댑터를 GridView 객체에 연결
         gridview.setAdapter(adapt);
 
+    }
+/*
+    private void getMonth(){
+        TextView todayDate = (TextView) findViewById(R.id.date);
+        Calendar today = Calendar.getInstance();
+        int year = today.get(Calendar.YEAR);
+        int month = today.get(Calendar.MONTH) +1;
+        todayDate.setText(year + "년" + month + "월");
 
     }
-
+*/
 
 
 }
