4e09487d662a2bd8b1e8e56482773f77eaee5352
yudini <oyj0922@naver.com>
2021-06-11 02:11:10 +0900

    지오코딩 성공 및 시작/종료 시간 정보 레이아웃 추가

--- app/src/main/java/com/hansung/android/myandroidapp/UserContract.java
+++ app/src/main/java/com/hansung/android/myandroidapp/UserContract.java
@@ -16,11 +16,20 @@ public final class UserContract {
         public static final String TABLE_NAME="Users";
         public static final String KEY_DATE="date";
         public static final String KEY_TITLE="title";
+        public static final String KEY_STARTTIME="startTime";
+        public static final String KEY_ENDTIME="endTime";
         public static final String KEY_ADDRESS="address";
         public static final String KEY_MEMO="memo";
 
-        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + _ID + " INTEGER PRIMARY KEY," + KEY_DATE + " TEXT," + KEY_TITLE + " TEXT," + KEY_ADDRESS + " TEXT," + KEY_MEMO + " TEXT)";
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+                KEY_DATE + TEXT_TYPE + COMMA_SEP +
+                KEY_TITLE + TEXT_TYPE + COMMA_SEP +
+                KEY_STARTTIME + TEXT_TYPE + COMMA_SEP +
+                KEY_ENDTIME + TEXT_TYPE + COMMA_SEP +
+                KEY_ADDRESS + TEXT_TYPE + COMMA_SEP +
+                KEY_MEMO + TEXT_TYPE + " )";
 
-        public static final String DELETE_TABLE="DROP TABLE IF EXISTS" +TABLE_NAME;
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
 }
