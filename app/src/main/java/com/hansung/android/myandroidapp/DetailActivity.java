41253d9ab2a4814c917a42d3a684a1c290980adb
yudini <oyj0922@naver.com>
2021-06-10 17:17:55 +0900

    데이터베이스 생성(오류)

--- app/src/main/java/com/hansung/android/myandroidapp/DetailActivity.java
+++ app/src/main/java/com/hansung/android/myandroidapp/DetailActivity.java
@@ -5,6 +5,7 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -14,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -27,31 +29,83 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
     private FusedLocationProviderClient mFusedLocationClient;
+    private EditText title;
     private Button findButton;
+    private Button save;
+    private Button delete;
+    private Button cancel;
     private EditText address ;
+    private String today;
+
     private Geocoder geocoder;
+    private DBHelper mDbHelper;
+    final int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 0;
+    Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        Intent intent = getIntent();
+        int year = intent.getIntExtra("year",0);
+        int month =intent.getIntExtra("month",0);
+        today = Integer.toString(year) + "년" +Integer.toString(month)+"월";
+
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+        title=findViewById(R.id.title);
         findButton = findViewById(R.id.search);
         address = findViewById(R.id.address);
+        save = (Button)findViewById(R.id.save);
+        cancel = (Button)findViewById(R.id.cancel);
+        delete = (Button)findViewById(R.id.delete);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertRecord();
+                finish();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mDbHelper = new DBHelper(this);
 
 
+        title.setText(year+"년"+month+"월");
         getLastLocation();
     }
 
-    final int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 0;
-    Location mLastLocation;
+    private void insertRecord(){
+        TextView memo = (TextView)findViewById(R.id.memo);
+        mDbHelper.insertUserBySQL(today,title.getText().toString(),address.getText().toString(),memo.getText().toString());
+    }
+
+    private void deleteRecord(){
+
+    }
+
 
     private void getLastLocation() {
         // 1. 위치 접근에 필요한 권한 검사 및 요청
